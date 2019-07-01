5b67a76bf469560a0458439f390089713ead99a5
Evans Shango <evanshango@github.com>
2019-07-01 22:38:04 +0300

    set up firebase authorization for user registration and login

---
+++ src/app/firebase.ts
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyCxyNUFUlLWRwasWv5c7lZpcCtXdl30Dr0",
+    authDomain: "social-app-aa4a9.firebaseapp.com",
+    databaseURL: "https://social-app-aa4a9.firebaseio.com",
+    projectId: "social-app-aa4a9",
+    storageBucket: "",
+    messagingSenderId: "535690507302",
+    appId: "1:535690507302:web:da420cf33abe0d64"
+}
+
+export default config
